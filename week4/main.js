70ec48450e34137b084bbf4745fa91d554317d14
ay2027 <ay2027@hw.ac.uk>
2023-11-03 09:43:02 +0400

    add some changes in the javascript

--- week4/main.js
+++ week4/main.js
@@ -3,6 +3,7 @@ var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://ay2027.github.io/week4/cities1.json');
 
 ourRequest.onload = function() {
-console.log(ourRequest.responseText);
+    var ourData = JSON.parse(ourRequest.responseText);
+    console.log(ourData[0]);
 };
 ourRequest.send();
